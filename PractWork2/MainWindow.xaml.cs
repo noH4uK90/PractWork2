eaeb6f74cc14282e66210afa3a3879d503c65099
noH4uK90 <noh4uk90@gmail.com>
2022-05-29 21:53:45 +0300

    Добавьте файлы проекта.

---
+++ PractWork2/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace PractWork2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MultiplyTableButton_Click(object sender, RoutedEventArgs e)
+        {
+            Excel.Application application = new();
+            Excel.Workbooks workbooks = application.Workbooks;
+            Excel.Workbook workbook = workbooks.Add();
+            Excel.Sheets worksheets = workbook.Worksheets;
+            Excel.Worksheet worksheet = worksheets.Item[1];
+            Excel.Range title = (Excel.Range) worksheet.get_Range("D9", "L9");
+            Excel.Range table = (Excel.Range) worksheet.get_Range("D10", "l18");
+            Excel.Range firstMyltiplier = (Excel.Range)worksheet.get_Range("E10", "l10");
+            Excel.Range secondMyltiplier = (Excel.Range)worksheet.get_Range("D11", "D18");
+            worksheet.Name = "Умножение";
+
+            title.Merge(Type.Missing);
+            title.Value = "Таблица умножения";
+            title.Font.Italic = true;
+            title.Font.Bold = true;
+            title.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            title.Font.Size = 20;
+
+            table.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            table.Font.Size = 15;
+            table.BorderAround2();
+
+            firstMyltiplier.Font.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.FromArgb(158, 253, 250));
+            firstMyltiplier.Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.FromArgb(46, 106, 133));
+            firstMyltiplier.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+            secondMyltiplier.Font.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.FromArgb(158, 253, 250));
+            secondMyltiplier.Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.FromArgb(46, 106, 133));
+            secondMyltiplier.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+
+            for (int i = 2; i < 10; i++)
+            {
+                table = worksheet.Cells[i + 9, 4];
+                table.Value = i;
+            }
+            for (int i = 2; i < 10; i++)
+            {
+                table = worksheet.Cells[10, i + 3];
+                table.Value = i;
+                for (int j = 2; j < 10; j++)
+                {
+                    table = worksheet.Cells[i + 9, j + 3];
+                    table.Value = i * j;
+                }
+            }
+            application.Visible = true;
+        }
+    }
+}
---
+++ Task4/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Spire.Xls;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Task4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var path = pathTextBox.Text;
+
+            if (Directory.Exists(path))
+            {
+                DirectoryInfo directory = new DirectoryInfo(path);
+                DirectoryInfo[] subDirectoryes = directory.GetDirectories("*", SearchOption.AllDirectories);
+                FileInfo[] files = directory.GetFiles();
+
+                Excel.Application application = new();
+                Excel.Workbooks workbooks = application.Workbooks;
+                Excel.Workbook workbook = workbooks.Add();
+                Excel.Sheets worksheets = workbook.Worksheets;
+                var directoryInfo = (Excel.Worksheet)worksheets.Add(Type.Missing, worksheets[1], Type.Missing, Type.Missing);
+                Excel.Worksheet filesInfo = worksheets.Item[1];
+                Excel.Range filesRange = (Excel.Range)filesInfo.get_Range("A1", "C" + files.Length + 1);
+                Excel.Range directoryRange = (Excel.Range)directoryInfo.get_Range("A1", "B" + subDirectoryes.Length + 1);
+                filesInfo.Name = "Файлы";
+                filesInfo.Cells[1, "A"].Value = "Номер";
+                filesInfo.Cells[1, "B"].Value = "Имя файла";
+                filesInfo.Cells[1, "C"].Value = "Размер в КБ";
+                directoryInfo.Name = "Папка";
+                directoryInfo.Cells[1, "A"].Value = "Номер";
+                directoryInfo.Cells[1, "B"].Value = "Имя папки";
+
+                for (int i = 1; i <= files.Length; i++)
+                {
+                    filesInfo.Cells[i + 1, "A"].Value = i;
+                    filesInfo.Cells[i + 1, "B"].Value = files[i - 1].Name;
+                    filesInfo.Cells[i + 1, "C"].Value = files[i - 1].Length / 1024;
+                }
+                filesInfo.Cells[files.Length + 2, "C"].Formula = $"=SUM(C2:C{files.Length + 1}) / 1024";
+                for (int i = 1; i <= subDirectoryes.Length; i++)
+                {
+                    directoryInfo.Cells[i + 1, "A"].Value = i;
+                    directoryInfo.Cells[i + 1, "B"].Value = subDirectoryes[i - 1].Name;
+                }
+                filesInfo.Columns.AutoFit();
+                directoryInfo.Columns.AutoFit();
+                application.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("Такой папки не существует");
+            }
+        }
+    }
+}
